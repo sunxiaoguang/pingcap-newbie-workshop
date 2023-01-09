f9e84199a41f118adbf01fc50d1dd539411eceaf
Xiaoguang Sun <sunxiaoguang@users.noreply.github.com>
2023-01-09 11:06:24 +0800

    Update pkg/cmp_and_add.go
    Co-authored-by: Lilian Lee <lilin@pingcap.com>

--- pkg/cmp_and_add.go
+++ pkg/cmp_and_add.go
@@ -1,6 +1,8 @@
 package pkg
 
-// No comments here
+// Compare two integers, then return the larger value if one is greater,
+// or the sum of the two values plus 10 if the second value is larger.
+// If the values are equal, return the sum of the two values.
 func CmpThenAdd(p1 int, p2 int) int {
 	if p1 > p2 {
 		return p1
